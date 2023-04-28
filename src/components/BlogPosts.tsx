b0304ff4d07d925d688ddf23213edc4ea3d8e841
Brendan Murphy <bmurf17@gmail.com>
2023-04-28 08:57:10 -0400

    Blog Post Filter Button

---
+++ src/components/BlogPosts.tsx
@@ -0,0 +1,74 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import type { MarkdownInstance } from "astro";
+import { useState } from "react";
+
+interface Props {
+  posts: MarkdownInstance<Record<string, any>>[];
+  tags: string[];
+}
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function BlogPosts({ posts, tags }: Props) {
+  const [filter, setFilter] = useState("");
+
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            {filter ? filter : "Filter"}
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="translate-x-36 absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1 ">
+              {tags?.map((tag) => {
+                return (
+                  <Menu.Item>
+                    {({ active }) => (
+                      <p
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                        onClick={() => {
+                          setFilter(tag);
+                        }}
+                      >
+                        {tag}
+                      </p>
+                    )}
+                  </Menu.Item>
+                );
+              })}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+      <ul>
+        {posts?.map((post) => {
+          return (
+            <li key={post.url}>
+              <a href={post.url}>{post.frontmatter.title}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
