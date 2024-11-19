675b9242808c1b67248a5a53fcb816537cb9c9de
bmurf17 <bmurf17@gmail.com>
2024-11-19 13:21:13 -0500

    making a cooler home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 20s linear infinite",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
